d00549817f301d72a25705896f26ed45f6242e4d
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-29 14:00:13 +0200

    Create columns component

---
+++ oddaj_rzeczy/src/components/home/columns/Columns.js
@@ -0,0 +1,32 @@
+export default function Columns() {
+    return (
+        <section className="columns">
+            <div className="columns__container">
+                <div className="columns__column">
+                    <h2 className="columns__number">10</h2>
+                    <h3 className="columns__title">oddanych worków</h3>
+                    <p className="columns__text">Lorem ipsum dolor sit amet,
+                        consectetur adipisc Pellentesque
+                        vel enim a elit viverra elementuma.
+                        Aliquam erat volutpat.</p>
+                </div>
+                <div className="columns__column">
+                    <h2 className="columns__number">5</h2>
+                    <h3 className="columns__title">wspratych organizacji</h3>
+                    <p className="columns__text">Lorem ipsum dolor sit amet,
+                        consectetur adipisc Pellentesque
+                        vel enim a elit viverra elementuma.
+                        Aliquam erat volutpat.</p>
+                </div>
+                <div className="columns__column">
+                    <h2 className="columns__number">7</h2>
+                    <h3 className="columns__title">zorganizowanych zbiórek</h3>
+                    <p className="columns__text">Lorem ipsum dolor sit amet,
+                        consectetur adipisc Pellentesque
+                        vel enim a elit viverra elementuma.
+                        Aliquam erat volutpat.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
