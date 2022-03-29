bbfd045778e5f70962497652a5ccad31bd0b4b30
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-29 15:45:42 +0200

    Add component About Us and add pariatl About Us; add styles to section about us

---
+++ oddaj_rzeczy/src/components/home/aboutUs/AboutUs.js
@@ -0,0 +1,27 @@
+import "./_aboutUs.scss";
+import Deco from "../../assets/Decoration.svg";
+import People from "../../assets/People.jpg";
+import Signature from "../../assets/Signature.svg";
+
+export default function AboutUs() {
+    return (
+        <section className="aboutUs">
+            <div className="aboutUs__content">
+                <h2 className="aboutUs__title">O nas</h2>
+                <img src={Deco} alt="Decoration" />
+                <p className="aboutUs__text">Nori grape silver beet broccoli
+                    kombu beet<br/>greens fava bean
+                    potato quandong celery.<br/>
+                    Bunya nuts black-eyed pea
+                    prairie turnip leek<br/>lentil
+                    turnip greens parsnip.</p>
+                <div className="aboutUs__signature">
+                    <img src={Signature} alt="Signature" />
+                </div>
+            </div>
+            <div className="aboutUs__image">
+                <img src={People} alt="People" />
+            </div>
+        </section>
+    )
+}
