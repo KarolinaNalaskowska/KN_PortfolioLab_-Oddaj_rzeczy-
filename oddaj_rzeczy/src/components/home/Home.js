e95e95c2613321f39df1b3d425ccd20ece7447e6
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-29 14:01:26 +0200

    Import columns to Home component

--- oddaj_rzeczy/src/components/home/Home.js
+++ oddaj_rzeczy/src/components/home/Home.js
@@ -1,9 +1,11 @@
 import Header from "./header/Header";
+import Columns from "./columns/Columns";
 
 export default function Home() {
     return (
         <>
             <Header />
+            <Columns />
         </>
     )
 }
