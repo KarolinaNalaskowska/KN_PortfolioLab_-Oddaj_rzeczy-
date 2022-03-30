696ab47906fcb07b25892d53e7032a4d004d851b
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-30 10:49:32 +0200

    Create component Login

--- oddaj_rzeczy/src/components/login/Login.js
+++ oddaj_rzeczy/src/components/login/Login.js
@@ -1,3 +1,34 @@
-export default function Login() {
+import Deco from "../../assets/Decoration.svg";
+import Nav from "../home/header/navigation/Nav";
 
+export default function Login() {
+    return (
+        <section>
+            <div>
+                <Nav />
+            </div>
+            <div>
+                <div>
+                    <h2>Zaloguj się</h2>
+                    <img src={Deco} alt="Decoration" />
+                </div>
+                <div>
+                    <form>
+                        <div>
+                            <p>Email</p>
+                            <input />
+                        </div>
+                        <div>
+                            <p>Hasło</p>
+                            <input />
+                        </div>
+                        <div>
+                            <button>Załóż konto</button>
+                            <button>Zaloguj się</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    )
 }
