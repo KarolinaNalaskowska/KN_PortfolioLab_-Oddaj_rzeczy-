c0af2347bbfa795678297cadd3bc27b5516023c4
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-30 10:30:10 +0200

    Import Component to App.js; Routes

--- oddaj_rzeczy/src/App.js
+++ oddaj_rzeczy/src/App.js
@@ -1,13 +1,25 @@
 import "./scss/main.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Logout from "./components/logout/Logout";
+import Register from "./components/register/Register";
 
 export default function App() {
   return (
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+          <Routes>
+              <Route path="/" element={<Home />} />
+          </Routes>
+          <Routes>
+              <Route path="/login" element={<Login />} />
+          </Routes>
+          <Routes>
+              <Route path="/logout" element={<Logout />} />
+          </Routes>
+          <Routes>
+              <Route path="/register" element={<Register />} />
+          </Routes>
       </BrowserRouter>
   );
 }
