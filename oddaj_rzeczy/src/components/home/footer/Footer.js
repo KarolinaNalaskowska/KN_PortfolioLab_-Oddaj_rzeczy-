a1a8b2c4af3a00cc276861113379db59fbdac665
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-30 10:07:35 +0200

    Add classes

--- oddaj_rzeczy/src/components/home/footer/Footer.js
+++ oddaj_rzeczy/src/components/home/footer/Footer.js
@@ -1,19 +1,19 @@
 import Fb from "../../../assets/Facebook.svg";
 import Ig from "../../../assets/Instagram.svg";
-import {FALSE} from "node-sass";
+import {Link} from "react-router-dom";
 
 export default function Footer() {
     return (
-        <div>
-            <div>
+        <div className="footer">
+            <div className="footer__text">
                 <p>Copyright by Coders Lab</p>
             </div>
-            <div>
-                <div>
-                    <img src={Fb} alt "Facebook icon" />
+            <div className="footer__icons">
+                <div className="footer__icons-fb">
+                    <Link to="https://www.facebook.com/"><img src={Fb} alt="Facebook icon" /></Link>
                 </div>
-                <div>
-                    <img src={Ig} alt="Instagram icon" />
+                <div className="footer__icons-ig">
+                    <Link to="https://www.instagram.com/"><img src={Ig} alt="Instagram icon" /></Link>
                 </div>
             </div>
         </div>
