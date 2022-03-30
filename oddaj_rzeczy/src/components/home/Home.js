cb3ea6746333530a6fe75281e3159a615a519338
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-30 10:02:05 +0200

    Import Footer component to Home

--- oddaj_rzeczy/src/components/home/Home.js
+++ oddaj_rzeczy/src/components/home/Home.js
@@ -4,6 +4,7 @@ import Steps from "./steps/Steps";
 import AboutUs from "./aboutUs/AboutUs";
 import Help from "./help/Help";
 import Contact from "./contact/Contact";
+import Footer from "./footer/Footer";
 
 export default function Home() {
     return (
@@ -25,6 +26,9 @@ export default function Home() {
             </>
             <>
                 <Contact />
+            </>
+            <>
+                <Footer />
             </>
         </>
     )
