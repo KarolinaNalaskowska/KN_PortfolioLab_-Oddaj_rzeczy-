895fd20e0b74267374dc1485b87a3e18195dd98f
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-30 12:16:30 +0200

    Change classes names

--- oddaj_rzeczy/src/components/login/Login.js
+++ oddaj_rzeczy/src/components/login/Login.js
@@ -4,31 +4,31 @@ import {Link} from "react-router-dom";
 
 export default function Login() {
     return (
-        <section className="login">
-            <div className="login__container">
+        <section className="logReg">
+            <div className="logReg__container">
                 <div className="navigation__login">
                     <Nav />
                 </div>
-                <div className="login__content">
-                    <div className="login__title">
-                        <h2 className="login__title-h2">Zaloguj się</h2>
+                <div className="logReg__content">
+                    <div className="logReg__title">
+                        <h2 className="logReg__title-h2">Zaloguj się</h2>
                         <img src={Deco} alt="Decoration" />
                     </div>
-                    <div className="formLogin">
-                        <form className="formLogin__form">
-                            <div className="formLogin__container">
-                                <div className="formLogin__inputs">
-                                    <p className="formLogin__p">Email</p>
-                                    <input className="formLogin__input" />
+                    <div className="formLogReg">
+                        <form className="formLogReg__form">
+                            <div className="formLogReg__container">
+                                <div className="formLogReg__inputs">
+                                    <p className="formLogReg__p">Email</p>
+                                    <input className="formLogReg__input" />
                                 </div>
-                                <div className="formLogin__inputs">
-                                    <p className="formLogin__p">Hasło</p>
-                                    <input className="formLogin__input" type="password" />
+                                <div className="formLogReg__inputs">
+                                    <p className="formLogReg__p">Hasło</p>
+                                    <input className="formLogReg__input" type="password" />
                                 </div>
                             </div>
-                            <div className="formLogin__buttons">
-                                <Link to="/register" className="login__btn">Załóż konto</Link>
-                                <Link to="/" className="login__btn">Zaloguj się</Link>
+                            <div className="formLogReg__buttons">
+                                <Link to="/register" className="logReg__btn">Załóż konto</Link>
+                                <Link to="/" className="logReg__btn">Zaloguj się</Link>
                             </div>
                         </form>
                     </div>
