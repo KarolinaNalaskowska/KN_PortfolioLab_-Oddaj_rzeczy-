3b7b8b905c7875fa0b443cc36b7aa825c11bdc30
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-30 12:05:53 +0200

    Add classes

--- oddaj_rzeczy/src/components/logout/Logout.js
+++ oddaj_rzeczy/src/components/logout/Logout.js
@@ -1,3 +1,22 @@
-export default function Logout() {
+import Nav from "../home/header/navigation/Nav";
+import Deco from "../../assets/Decoration.svg";
+import {Link} from "react-router-dom";
 
+export default function Logout() {
+    return (
+        <section className="logout">
+            <div className="logout__container">
+                <div className="navigation__login">
+                    <Nav />
+                </div>
+                <div className="logout__content">
+                    <div className="logout__title">
+                        <h2 className="logout__title-h2">Wylogowanie nastąpiło<br/>pomyślnie!</h2>
+                        <img src={Deco} alt="Decoration" className="logout__deco"/>
+                    </div>
+                    <Link to="/" className="logout__button">Strona główna</Link>
+                </div>
+            </div>
+        </section>
+    )
 }
