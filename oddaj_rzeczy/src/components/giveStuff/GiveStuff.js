6b51440123e264fe467fefe96753ec3dca68c27f
Karolina <nalaskowska.karolina3@gmail.com>
2022-04-07 11:35:12 +0200

    Create structure of GiveStuff component

--- oddaj_rzeczy/src/components/giveStuff/GiveStuff.js
+++ oddaj_rzeczy/src/components/giveStuff/GiveStuff.js
@@ -1,9 +1,55 @@
 import GiveStuff_Form from "./GiveStuff_Form";
+import HeaderBackground from "../../assets/Header-Form-Background.png"
+import Deco from "../../assets/Decoration.svg";
+import Nav from "../home/navigation/Nav";
+import Contact from "../home/contact/Contact";
 
 export default function GiveStuff() {
     return (
-        <div>
-            <GiveStuff_Form />
-        </div>
+        <>
+            <header>
+                <div>
+                    <div>
+                        <img src={HeaderBackground} alt="Background Header" />
+                    </div>
+                    <div>
+                        <div>
+                            <Nav />
+                        </div>
+                        <div>
+                            <div>
+                                <h2>Oddaj/</h2>
+                            </div>
+                            <div>
+                                <img src={Deco} alt="Decoration" />
+                            </div>
+                            <div>
+                                <h3>wystarcza</h3>
+                            </div>
+                            <div>
+                                <div>1</div>
+                                <div>2</div>
+                                <div>3</div>
+                                <div>4</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <section>
+                <div>
+                    <h2>Ważne!</h2>
+                    <p>Wszystkie rzeczy do oddania zapakuj w 60l worki. Dokładną instrukcję jak poprawnie spakować rzeczy znajdziesz TUTAJ.</p>
+                </div>
+            </section>
+            <main>
+                <section>
+                    <GiveStuff_Form />
+                </section>
+            </main>
+            <>
+                <Contact />
+            </>
+        </>
     )
 }
