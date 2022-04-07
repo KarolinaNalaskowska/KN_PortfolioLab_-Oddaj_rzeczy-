0c7f895d87199316b108e03b1fee4dadb019079a
Karolina <nalaskowska.karolina3@gmail.com>
2022-04-07 13:06:19 +0200

    Add classes and styles to giveStuff component

--- oddaj_rzeczy/src/components/giveStuff/GiveStuff.js
+++ oddaj_rzeczy/src/components/giveStuff/GiveStuff.js
@@ -7,30 +7,44 @@ import Contact from "../home/contact/Contact";
 export default function GiveStuff() {
     return (
         <>
-            <header>
-                <div>
-                    <div>
+            <header className="stuffHeader">
+                <div className="stuffHeader__container">
+                    <div className="stuffHeader__image">
                         <img src={HeaderBackground} alt="Background Header" />
                     </div>
-                    <div>
+                    <div className="stuffHeader__right">
                         <div>
                             <Nav />
                         </div>
-                        <div>
-                            <div>
-                                <h2>Oddaj/</h2>
+                        <div className="stuffHeader__content">
+                            <div className="stuffHeader__title">
+                                <h2 className="stuffHeader__title-h2">Oddaj rzeczy, których już nie chcesz<br/>
+                                    <span>potrzebującym</span>
+                                </h2>
                             </div>
-                            <div>
+                            <div className="stuffHeader__deco">
                                 <img src={Deco} alt="Decoration" />
                             </div>
-                            <div>
-                                <h3>wystarcza</h3>
+                            <div className="stuffHeader__title">
+                                <h3 className="stuffHeader__title-h3">Wystarczą 4 proste kroki:</h3>
                             </div>
-                            <div>
-                                <div>1</div>
-                                <div>2</div>
-                                <div>3</div>
-                                <div>4</div>
+                            <div className="stuffHeader__rhombuses">
+                                <div className="stuffHeader__rhombus stuffHeader__rhombus-1">
+                                    <p className="stuffHeader__rhombus-content stuffHeader__rhombus-number">1</p>
+                                    <p className="stuffHeader__rhombus-content stuffHeader__rhombus-text">Wybierz<br/>rzeczy</p>
+                                </div>
+                                <div className="stuffHeader__rhombus stuffHeader__rhombus-2">
+                                    <p className="stuffHeader__rhombus-content stuffHeader__rhombus-number">2</p>
+                                    <p className="stuffHeader__rhombus-content stuffHeader__rhombus-text">Spakuj je<br/>w worki</p>
+                                </div>
+                                <div className="stuffHeader__rhombus stuffHeader__rhombus-3">
+                                    <p className="stuffHeader__rhombus-content stuffHeader__rhombus-number">3</p>
+                                    <p className="stuffHeader__rhombus-content stuffHeader__rhombus-text">Wybierz<br/>fundację</p>
+                                </div>
+                                <div className="stuffHeader__rhombus stuffHeader__rhombus-4">
+                                    <p className="stuffHeader__rhombus-content stuffHeader__rhombus-number">4</p>
+                                    <p className="stuffHeader__rhombus-content stuffHeader__rhombus-text">Zamów<br/>kuriera</p>
+                                </div>
                             </div>
                         </div>
                     </div>
